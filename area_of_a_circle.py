a910345c119a22d739b1d997a94858cab477a742
nguyenminhvinhky <NguyenMinhVinhKy@gmail.com>
2023-02-01 19:02:25 -0600

    change radius to diameter

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,12 +1,12 @@
-def compute_area_of_circle(radius):
+def compute_area_of_circle(diameter):
 	pi = 3.14
-	area = pi * radius * radius
+	area = (pi * (diameter ** 2))/4
 	return area
 
-radius1 = 30
-area1 = compute_area_of_circle(radius1)
-print(f"The area of circle with radius {radius1} is: {area1}")
+diameter1 = 30
+area1 = compute_area_of_circle(diameter1)
+print(f"The area of circle with diameter {diameter1} is: {area1}")
 
-radius2 = 40
-area2 = compute_area_of_circle(radius2)
-print(f"The area of circle with radius {radius2} is: {area2}")
+diameter2 = 40
+area2 = compute_area_of_circle(diameter2)
+print(f"The area of circle with diameter {diameter2} is: {area2}")
